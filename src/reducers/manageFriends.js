15228701ff2c8640f9041c28c2657bfdfe3c7f78
Nate Miller <natekmiller96@gmail.com>
2018-11-16 20:11:43 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return {friends: ...state
+      return {friends: ...state.friends
     default:
       return state;
   }  
