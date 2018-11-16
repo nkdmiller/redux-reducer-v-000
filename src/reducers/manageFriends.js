65d722dd4da9ce3ff6c76898b9f544e1d4554dbf
Nate Miller <natekmiller96@gmail.com>
2018-11-16 20:04:18 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,8 @@
 export function manageFriends(state, action){
-  
+  switch (action.type) {
+    case 'INCREASE':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }  
 }
