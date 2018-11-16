b136f3019eb4b20aceaa779e1838f6b70c532324
Nate Miller <natekmiller96@gmail.com>
2018-11-16 20:03:09 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,7 @@
 export function managePresents(state, action){
   switch (action.type) {
     case 'INCREASE':
-      return {count: state.numberOfPresents + 1}
+      return {numberOfPresents: state.numberOfPresents + 1}
     default:
       return state;
   }
